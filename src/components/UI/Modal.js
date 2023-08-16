84aa6fdd75a4289eb866f7fd202af89c3ba3bbc3
Uday Gohel <goheluday6445@gmail.com>
2023-08-16 13:57:02 +0530

    ..

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -8,8 +8,16 @@ import {
   TEModalBody,
   TEModalFooter,
 } from "tw-elements-react";
+import classes from "../Cart/Cart.module.css";
 
 const Modal = (props) => {
+  const cartItems = (
+    <ul>
+      {[{ id: "c1", name: "Shusi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
   return (
     <div>
       ;{/* <!-- Modal --> */}
@@ -19,7 +27,7 @@ const Modal = (props) => {
             <TEModalHeader>
               {/* <!--Modal title--> */}
               <h5 className="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200">
-                Modal title
+                {cartItems}
               </h5>
               {/* <!--Close button--> */}
               <button
@@ -45,7 +53,12 @@ const Modal = (props) => {
               </button>
             </TEModalHeader>
             {/* <!--Modal body--> */}
-            <TEModalBody>Modal body text goes here.</TEModalBody>
+            <TEModalBody>
+              <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>35.25</span>
+              </div>
+            </TEModalBody>
             <TEModalFooter>
               <TERipple rippleColor="light">
                 <button
@@ -61,7 +74,7 @@ const Modal = (props) => {
                   type="button"
                   className="ml-1 inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                 >
-                  Save changes
+                  Order
                 </button>
               </TERipple>
             </TEModalFooter>
