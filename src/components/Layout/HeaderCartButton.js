84bac364ec06a714fb84821ec41cb3ea446fe918
Uday Gohel <goheluday6445@gmail.com>
2023-08-17 09:17:29 +0530

    Completed with Animation in Button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,36 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [highlitedBtn, setHighlitedBtn] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberofCartItems = cartCtx.items.reduce((currentNumber, item) => {
+  const { items } = cartCtx;
+  const numberofCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${highlitedBtn ? classes.bump : ""} `;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setHighlitedBtn(true);
+
+    const timer = setTimeout(() => {
+      setHighlitedBtn(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.show}>
+    <button className={btnClasses} onClick={props.show}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
