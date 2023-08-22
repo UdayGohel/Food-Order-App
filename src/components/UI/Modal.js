d817b3aea6faaa6dc5b38ad374602d00de4e3f45
Uday Gohel <goheluday6445@gmail.com>
2023-08-22 10:59:45 +0530

    Order Submitted iin FireBase

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { TERipple } from "tw-elements-react";
 import {
   TEModal,
@@ -11,8 +11,12 @@ import {
 import classes from "../Cart/Cart.module.css";
 import CartContext from "../../store/cart-context";
 import CartItem from "../Cart/CartItem";
+import Checkout from "../Cart/Checkout";
 
 const Modal = (props) => {
+  const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -24,6 +28,24 @@ const Modal = (props) => {
   const cartItemAddHandler = (item) => {
     cartCtx.addItem({ ...item, amount: 1 });
   };
+
+  const submitorderHandler = async (userData) => {
+    setIsSubmitting(true);
+    const res = await fetch(
+      "https://react-foodorder-5da2d-default-rtdb.firebaseio.com/orders.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          user: userData,
+          orderedItems: cartCtx.items,
+        }),
+      }
+    );
+    setIsSubmitting(false);
+    setDidSubmit(true);
+    cartCtx.clearCart();
+  };
+
   const cartItems = (
     <ul>
       {cartCtx.items.map((item) => (
@@ -38,70 +60,127 @@ const Modal = (props) => {
       ))}
     </ul>
   );
+
+  const orderHandler = () => {
+    setIsCheckout(true);
+  };
+  const modalActions = (
+    <TEModalFooter>
+      <TERipple rippleColor="light">
+        <button
+          type="button"
+          className="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
+          onClick={props.toggle}
+        >
+          Close
+        </button>
+      </TERipple>
+      <TERipple rippleColor="light">
+        {hasItems && (
+          <button
+            type="button"
+            onClick={orderHandler}
+            className="ml-1 inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+          >
+            Order
+          </button>
+        )}
+      </TERipple>
+    </TEModalFooter>
+  );
+
+  const cartModalContent = (
+    <TEModalDialog>
+      <TEModalContent>
+        <TEModalHeader>
+          {/* <!--Modal title--> */}
+          <h5 className="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200">
+            {cartItems}
+          </h5>
+          {/* <!--Close button--> */}
+          <button
+            type="button"
+            className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+            onClick={props.toggle}
+            aria-label="Close"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </TEModalHeader>
+        {/* <!--Modal body--> */}
+        <TEModalBody>
+          <div className={classes.total}>
+            <span>Total Amount</span>
+            <span>{totalAmount}</span>
+          </div>
+          {isCheckout && (
+            <Checkout onConfirm={submitorderHandler} onCancel={props.toggle} />
+          )}
+          {!isCheckout && modalActions}
+        </TEModalBody>
+      </TEModalContent>
+    </TEModalDialog>
+  );
+
+  const isSubmittingModalContent = (
+    <TEModalContent>
+      <TEModalHeader>
+        <h5 className="text-xl text-center font-medium leading-normal text-neutral-800 dark:text-neutral-200">
+          Sending order data...
+        </h5>
+      </TEModalHeader>
+    </TEModalContent>
+  );
+  const didSubmitModalContent = (
+    <TEModalContent>
+      <TEModalHeader>
+        <h5 className="text-xl text-center font-medium leading-normal text-neutral-800 dark:text-neutral-200">
+          Successfully send the order!
+        </h5>
+        <button
+          type="button"
+          className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+          onClick={props.toggle}
+          aria-label="Close"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </TEModalHeader>
+    </TEModalContent>
+  );
+
   return (
     <div>
-      ;{/* <!-- Modal --> */}
       <TEModal show={props.show} setShow={props.setShow}>
-        <TEModalDialog>
-          <TEModalContent>
-            <TEModalHeader>
-              {/* <!--Modal title--> */}
-              <h5 className="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200">
-                {cartItems}
-              </h5>
-              {/* <!--Close button--> */}
-              <button
-                type="button"
-                className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
-                onClick={props.toggle}
-                aria-label="Close"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="h-6 w-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </TEModalHeader>
-            {/* <!--Modal body--> */}
-            <TEModalBody>
-              <div className={classes.total}>
-                <span>Total Amount</span>
-                <span>{totalAmount}</span>
-              </div>
-            </TEModalBody>
-            <TEModalFooter>
-              <TERipple rippleColor="light">
-                <button
-                  type="button"
-                  className="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
-                  onClick={props.toggle}
-                >
-                  Close
-                </button>
-              </TERipple>
-              <TERipple rippleColor="light">
-                {hasItems && (
-                  <button
-                    type="button"
-                    className="ml-1 inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                  >
-                    Order
-                  </button>
-                )}
-              </TERipple>
-            </TEModalFooter>
-          </TEModalContent>
-        </TEModalDialog>
+        {!isSubmitting && !didSubmit && cartModalContent}
+        {isSubmitting && isSubmittingModalContent}
+        {!isSubmitting && didSubmit && didSubmitModalContent}
       </TEModal>
     </div>
   );
